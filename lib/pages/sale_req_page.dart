2d8c273454b5cb0baf436892a031fb8f9e354186
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-02 20:15:57 +0600

    Sale Request Page #2

--- lib/pages/sale_req_page.dart
+++ lib/pages/sale_req_page.dart
@@ -8,8 +8,55 @@ class SaleRequestPage extends StatefulWidget {
 }
 
 class _SaleRequestPageState extends State<SaleRequestPage> {
+
+  // Initial Selected Value
+  String dropdownvalue = 'Item 1';
+
+  // List of items in our dropdown menu
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sale Request Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton(
+
+              // Initial Value
+              value: dropdownvalue,
+
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),
+
+              // Array list of items
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
