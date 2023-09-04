006623a6d6082942dfd524b805a1e0c60b95126f
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-04 18:22:46 +0600

    Product Category

---
+++ lib/models/product_category.dart
@@ -0,0 +1,26 @@
+class Category {
+  String? name;
+  String? image;
+
+  Category({this.name, this.image});
+
+  Category.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    image = json['Image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['Image'] = this.image;
+    return data;
+  }
+}
+
+final List<Category> categoryList = [
+  Category(name: "Cat",image: "images/cat logo.png"),
+  Category(name: "Dog",image: "images/dog logo.png"),
+  Category(name: "Bird",image: "images/bird logo.png"),
+  Category(name: "Rabbit",image: "images/rabbit.png"),
+
+];
