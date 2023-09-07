d7bd1695ce34de1869225f170584203035c8c100
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-07 23:01:44 +0600

    Profile Page #9

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:client_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -9,62 +12,81 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  List<File> selectedImages = [];
+  final picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
+    // display image selected from gallery
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Profile'),
-        leading: InkWell(
-            onTap :(){
-              Navigator.pop(context);
-              print('This is On Tap');
-            },
-            child: Icon(Icons.arrow_back)),
+        title: const Text('Profile Page'),
+        backgroundColor: Colors.green,
+        actions: const [],
       ),
-      body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-        TextFormField(
-        decoration: const InputDecoration(
-          icon: Icon(Icons.person),
-        hintText: 'Enter Your Name',
-        labelText: 'Name',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green)),
+              child: const Text('Select Image from Gallery and Camera'),
+              onPressed: () {
+                getImages();
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 18.0),
+              child: Text(
+                "GFG",
+                textScaleFactor: 3,
+                style: TextStyle(color: Colors.green),
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                width: 300.0,
+                child: selectedImages.isEmpty
+                    ? const Center(child: Text('Sorry nothing selected!!'))
+                    : GridView.builder(
+                  itemCount: selectedImages.length,
+                  gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Center(
+                        child: kIsWeb
+                            ? Image.network(selectedImages[index].path)
+                            : Image.file(selectedImages[index]));
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.phone),
-                  hintText: 'Enter Phone Number',
-                  labelText: 'Phone',
-                ),
-              ),
-              TextFormField(
-              keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.email),
-                  hintText: 'Enter Your Email',
-                  labelText: 'Email',
-                ),
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.man_sharp),
-                  hintText: 'Enter Your NID Number',
-                  labelText: 'NID',
-                ),
-              ),
-              Container(
-                  padding: const EdgeInsets.only(left: 150.0, top: 40.0),
-                  child: ElevatedButton(
-                    onPressed: (){},
-                    child: const Text('Submit'),
-                  ),
-              ),
-            ]
-    ),
+    );
+  }
 
+  Future getImages() async {
+    final pickedFile = await picker.pickMultiImage(
+        imageQuality: 100, maxHeight: 1000, maxWidth: 1000);
+    List<XFile> xfilePick = pickedFile;
+
+    setState(
+          () {
+        if (xfilePick.isNotEmpty) {
+          for (var i = 0; i < 1; i++) {
+            selectedImages.add(File(xfilePick[i].path));
+          }
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Nothing is selected')));
+        }
+      },
     );
   }
 }
