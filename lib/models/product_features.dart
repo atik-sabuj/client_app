966a75781ccab429bc59d141a531d988194f1d2c
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-10 23:23:32 +0600

    Product Features#2

--- lib/models/product_features.dart
+++ lib/models/product_features.dart
@@ -26,3 +26,57 @@ final List<Features> featuresList = [
   Features(name: "Smart Phones",image: "images/phone.png", price: 5000.0,),
 
 ];
+
+
+
+
+
+
+
+Features({this.name, this.image,  this.price,});
+
+Features.fromJson(Map<String, dynamic> json) {
+name = json['name'];
+image = json['Image'];
+price = json['price'];
+
+}
+
+Map<String, dynamic> toJson() {
+  final Map<String, dynamic> data = new Map<String, dynamic>();
+  data['name'] = this.name;
+  data['Image'] = this.image;
+  data['price'] = this.price;
+  return data;
+}
+}
+
+final List<Features> featuresList = [
+  Features(name: "Smart Phones",image: "images/phone.png", price: 5000.0,),
+
+];
+
+
+
+Features({this.name, this.image,  this.price,});
+
+Features.fromJson(Map<String, dynamic> json) {
+name = json['name'];
+image = json['Image'];
+price = json['price'];
+
+}
+
+Map<String, dynamic> toJson() {
+  final Map<String, dynamic> data = new Map<String, dynamic>();
+  data['name'] = this.name;
+  data['Image'] = this.image;
+  data['price'] = this.price;
+  return data;
+}
+}
+
+final List<Features> featuresList = [
+  Features(name: "Smart Phones",image: "images/phone.png", price: 5000.0,),
+
+];
